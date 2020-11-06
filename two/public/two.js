223d05e89d7c1c46e32ad068c6e1290353482399
Maximiliano Usich <maximiliano.usich@softvision.com>
2020-11-06 12:19:56 -0300

    feat(tsc): html

--- two/public/two.js
+++ two/public/two.js
@@ -1,3 +1,6 @@
 "use strict";
 var theArticles = document.querySelectorAll('article');
 theArticles.forEach(function (e) { return console.log(e); });
+var elLasagna = document.querySelector('#lasagna');
+console.log("" + elLasagna);
+console.log("" + JSON.stringify(elLasagna));
--- two/src/two.ts
+++ two/src/two.ts
@@ -1,3 +1,7 @@
 
 const theArticles = document.querySelectorAll('article');
 theArticles.forEach( (e) => console.log(e));
+
+const elLasagna = document.querySelector('#lasagna') as HTMLElement;
+console.log(`${elLasagna}`);
+console.log(`${JSON.stringify(elLasagna)}`);
