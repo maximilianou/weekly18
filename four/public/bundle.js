3ba94460b9c39d98db70e177b47b0aef06a717c7
Maximiliano Usich <maximiliano.usich@softvision.com>
2020-11-09 15:22:20 -0300

    fix(env): form to console, typescript over webpack

---
+++ four/public/bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("running from ts to js..")})();
---
+++ four/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            { 
+                test: /\.ts$/,
+                use: 'ts-loader',
+                include: [ path.resolve(__dirname, 'src')  ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        publicPath: 'public',
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    devServer: {
+        contentBase: [
+            path.join(__dirname, 'public'), 
+            path.join(__dirname, 'assets')
+        ],
+        port: 9000
+    }
+}
