d0df715911bf729549941340f2788ee2bc17b9c5
Maximiliano Usich <maximiliano.usich@softvision.com>
2020-11-09 14:27:42 -0300

    fix(env): webpack dev server, npm run serve

--- three/webpack.config.js
+++ three/webpack.config.js
@@ -18,5 +18,8 @@ module.exports = {
         publicPath: 'public',
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public'),
+    },
+    devServer: {
+        port: 9000
     }
 }
