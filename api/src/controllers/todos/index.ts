d8e12a51057834af55f571ae4791ca945b20e7db
Maximiliano Usich <maximiliano.usich@softvision.com>
2020-11-12 17:01:00 -0300

    wip(api): adding delete

--- api/src/controllers/todos/index.ts
+++ api/src/controllers/todos/index.ts
@@ -53,5 +53,22 @@ const updateTodo = async (req: Request, res: Response):Promise<void> => {
 }
 
 
+const deleteTodo = async (req: Request, res: Response):Promise<void> => {
+  try{
+    const deleteTodo: ITodo | null = await Todo.findByIdAndRemove(
+      req.params.id
+    );
+    const allTodos: ITodo[] = Todo.find();
+    res.status(200).json({
+      message: "Todo deleted",
+      todo: deletedTodo,
+      todos: allTodos,
+    });
+    
+  }catch(err){
+    throw err;
+  }
+}
 
 
+export { getTodos, addTodo, updateTodo, deleteTodo };
